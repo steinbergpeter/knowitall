01f3a581dc88a92f79a4c37929c18f7f86120654
Peter Steinberg <bklynpeter@gmail.com>
2025-06-05 22:03:33 -0400

    fix param in project-detail-page

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -3,13 +3,13 @@ import { ProjectDocumentList } from './_components/project-document-list'
 import ProjectDetailClient from './_components/project-detail-client'
 
 interface ProjectDetailPageProps {
-  params: {
+  params: Promise<{
     projectId: string
-  }
+  }>
 }
 
 export default async function ProjectDetailPage(props: ProjectDetailPageProps) {
-  const { projectId } = props.params
+  const { projectId } = await props.params
   return (
     <main className="flex flex-col items-center justify-start min-h-screen py-12">
       <ProjectDetailClient projectId={projectId} />
